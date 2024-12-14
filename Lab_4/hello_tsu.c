80e9cc2771ff4b685c484c86707ae4c0605d9407
Your-vo1d <my.vo1d.edge@gmail.com>
2024-12-14 05:48:34 +0000

    Fix time

--- Lab_4/hello_tsu.c
+++ Lab_4/hello_tsu.c
@@ -3,8 +3,7 @@
 #include <linux/kernel.h>  
 #include <linux/proc_fs.h>
 #include <linux/version.h>
-#include <linux/time.h>
-#include <linux/ktime.h>
+#include <linux/uaccess.h>  // Для copy_to_user
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Likontsev Nikolay aka Your_Vo1d"); 
@@ -17,24 +16,46 @@ MODULE_VERSION("1.0");
 static struct proc_dir_entry *our_proc_file = NULL;
 
 static ssize_t procfile_read(
-    struct file *file_pointer, char __user *buffer, size_t buffer_length, loff_t* offset)
-    {
-        struct tm date;
-        date.tm_year = 2024 - 1900;
-        date.tm_mon = 11;
-        date.tm_mday = 31;
-        date.tm_hour = 23;
-        date.tm_min = 59;
-        date.tm_sec = 59;
-        return 0; // Временно
+    struct file *file_pointer, char __user *buffer, size_t buffer_length, loff_t *offset)
+{
+    time64_t target_time = 1735689599; // Целевое время (в секундах UTC)
+    time64_t current_time;
+    time64_t diff_time;
+
+    char result[100];
+    ssize_t len;
+
+    // Получаем текущее время UTC
+    current_time = ktime_get_real_seconds();
+
+
+    // Рассчитываем разницу во времени
+    diff_time = target_time - current_time;
+
+    if (*offset > 0)
+        return 0;
+
+    // Форматируем разницу в строку
+    len = snprintf(result, sizeof(result), "%lld\n", diff_time);
+
+    if (buffer_length < len)
+        return -EINVAL;
+
+    if (copy_to_user(buffer, result, len)) {
+        pr_warn("Ошибка копирования\n");
+        return -EFAULT;
     }
+
+    *offset += len;
+    return len;
+}
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
 static const struct proc_ops proc_file_fops = {
-    .proc_read= procfile_read,
+    .proc_read = procfile_read,
 };
 #else
-static const struct file_operations_proc_file_fops = {
+static const struct file_operations proc_file_fops = {
     .read = procfile_read,
 };
 #endif
@@ -42,14 +63,15 @@ static const struct file_operations_proc_file_fops = {
 // Функция загрузки модуля
 static int __init tsu_module_init(void) {
     our_proc_file = proc_create(procfs_name, 0644, NULL, &proc_file_fops);
+
     pr_info("Welcome to the Tomsk State University\n");
-    return 0; 
+    return 0;
 }
 
 // Функция выгрузки модуля
 static void __exit tsu_module_exit(void) {
     proc_remove(our_proc_file);
-    pr_info("/proc/%s removed", procfs_name)
+    pr_info("/proc/%s removed\n", procfs_name);
     pr_info("Tomsk State University forever!\n");
 }
 
